542369f6c90dc73e182f618207328649dc29f207
Hongbo LU <gitawego@gmail.com>
2017-03-22 23:32:19 +0100

    wrong types

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,17 +1,18 @@
+import {CountryActionType} from './actions';
 export default function reducer(state, action) {
   switch (action.type) {
-    case 'ALL_COUNTRIES_LOADING':
+    case CountryActionType.ALL_COUNTRIES_LOADING:
       return {
         ...state,
         countriesLoading: true
       };
-    case 'ALL_COUNTRIES_LOADED':
+    case CountryActionType.ALL_COUNTRIES_LOADED:
       return {
         ...state,
         countriesLoading: false,
         countries: action.payload,
       };
-    case 'NAME_CHANGED':
+    case CountryActionType.NAME_CHANGED:
       return {
         ...state,
         name: action.payload
