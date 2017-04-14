92001dd171d47168f27ed92742c121f5988e906f
Hongbo LU <gitawego@gmail.com>
2017-04-14 08:52:36 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -9,7 +9,7 @@ export interface ReduxStore {
   dispatch(type: string, payload?: any): void;
   getState(): any;
   subscribe(listener: (event) => void): () => void;
-  select(...keys: string[]): Observable<any>;
+  select<T>(...keys: string[]): Observable<T>;
 }
 export interface State {
   [id: string]: any;
