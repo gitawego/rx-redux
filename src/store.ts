1b20a6a175acc585ee5b1085389dca4dd7fe7bfb
Hongbo LU <gitawego@gmail.com>
2017-04-19 10:09:22 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,34 +1,27 @@
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/startWith';
 import { isObservable } from './utils';
 
+
+//export { State };
 export interface Action {
   type: string;
   payload?: any;
 }
+export type Reducer<S> = (state: S, action: Action) => S;
 export interface ReduxStore<S> {
   dispatch(type: string, payload?: any): void;
   getState(): S;
-  subscribe(listener: (event) => void): () => void;
+  subscribe(listener: (state: S) => void): () => void;
   select<T>(...keys: string[]): Observable<T>;
 }
-export interface State {
-  [id: string]: any;
-}
-
-export type Reducer = (state: any, action: Action) => any;
 
 const action$ = new Subject();
 let store = null;
-export const initStore = (initState: State, reducer: Reducer) => {
-  return action$
-    .flatMap((action: any) => isObservable(action) ? action : Observable.of(action))
-    .startWith(initState)
-    .scan(reducer)
-    // these two lines make our observable hot and have it emit the last state
-    // upon subscription
-    .publishReplay(1)
-    .refCount();
-};
 
 export const dispatch = (type: string, payload?: any) => {
   const action: Action = {
@@ -36,40 +29,53 @@ export const dispatch = (type: string, payload?: any) => {
     payload,
   };
   action$.next(action);
-
   if (isObservable(action.payload)) {
     action$.next(action.payload);
   }
 };
 
-export const createStore = (initState: State, reducer: Reducer): ReduxStore<State> => {
+export function initStore<S>(initState: S, reducer: Reducer<S>): Observable<S> {
+  return action$
+    .flatMap((action: any) => isObservable(action) ? action : Observable.of(action))
+    .scan(reducer, initState)
+    // these two lines make our observable hot and have it emit the last state
+    // upon subscription
+    .publishReplay(1)
+    .refCount();
+};
+
+export function createStore<S>(initState: S, reducer: Reducer<S>): ReduxStore<S> {
   if (store) {
     throw new Error('store is already created');
   }
   const stateHolder = {
     state: initState
   };
-  store = initStore(initState, reducer);
-  store.subscribe((state) => {
-    // stateStore.next(state);
-    stateHolder.state = state;
-  });
+  store = initStore<S>(initState, reducer);
+  store
+    .do(state => {
+      if (ENV === 'development') {
+        console.log('store debug', JSON.parse(JSON.stringify(state)));
+      }
+    })
+    .subscribe((state: S) => {
+      stateHolder.state = state;
+    });
   return {
     dispatch,
-    getState(): State {
-      return { ...stateHolder.state };
+    getState(): S {
+      return stateHolder.state;
     },
     subscribe(listener) {
       const sub = store.map(() => stateHolder.state)
         .subscribe(listener);
       return () => sub.unsubscribe();
     },
-    select(...keys: string[]) {
+    select<T>(...keys: string[]): Observable<T> {
       return store.map(() => stateHolder.state)
         .pluck(...keys)
         .distinctUntilChanged();
-
-    },
+    }
   };
 };
 
@@ -87,10 +93,11 @@ export const actionCreator = (func) => (...args) => {
  * this method can combine multiple reducers, then passes to method createStore
  * @param reducers
  */
-export const combineReducers = (reducers: Reducer[]) =>
-  (state, action): State => {
+export function combineReducers<S>(reducers: Array<Reducer<any>>) {
+  return (state, action): S => {
     reducers.forEach((reducer) => {
       state = reducer(state, action);
     });
     return state;
   };
+};
