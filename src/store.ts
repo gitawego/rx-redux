e4598a8d8db6013383cd422632528d1d9ce26afd
Hongbo LU <gitawego@gmail.com>
2017-05-30 11:08:08 +0200

    Create store.ts

---
+++ src/store.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs/Observable';
+export class Store<T> extends Observable<T> {
+  constructor(
+    $state: Observable<any>
+  ) {
+    super();
+    this.source = $state;
+  }
+  getState() {
+    return { ...this.source['stateValue'] };
+  }
+  select<S>(...keys: string[]): Observable<S> {
+    return this.pluck(...keys)
+      .distinctUntilChanged();
+  }
+}
