d37406a1852cecb8fda95876c12ad6d913d80730
Hongbo LU <gitawego@gmail.com>
2017-05-31 23:54:53 +0200

    updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 a simple rxjs based redux inspired by [rxjs-as-redux](https://github.com/ryardley/rxjs-as-redux) and [ngrx/store](https://github.com/ngrx/store)
 
-
+``reflect-metadata`` is required for effect
 ## usage
 
 see ``examples`` folder
--- src/effect.ts
+++ src/effect.ts
@@ -1,18 +1,21 @@
-import "reflect-metadata";
 import { action$ } from './redux';
 
 const EFFECT_KEY = Symbol('effect');
+
+/**
+ * effect requires ``reflect-metadata``
+ */
 export function Effect() {
   return function (target: any, propertyName: string) {
-    const effectProperties = Reflect.getOwnMetadata(EFFECT_KEY, target) || [];
+    const effectProperties = (Reflect as any).getOwnMetadata(EFFECT_KEY, target) || [];
     effectProperties.push(propertyName);
-    Reflect.defineMetadata(EFFECT_KEY, effectProperties, target);
+    (Reflect as any).defineMetadata(EFFECT_KEY, effectProperties, target);
   }
 }
 
 export function addEffect(instance) {
   const target = Object.getPrototypeOf(instance);
-  const effectProperties = Reflect.getOwnMetadata(EFFECT_KEY, target) || [];
+  const effectProperties = (Reflect as any).getOwnMetadata(EFFECT_KEY, target) || [];
   effectProperties.forEach(name => {
     instance[name].subscribe(action$.dispatch.bind(action$));
   });
